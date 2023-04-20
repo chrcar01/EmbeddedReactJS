ecf2f9d827ec9102b4be53e4f8230f73a6b49374
chrcar01 <chrisjcarter@gmail.com>
2023-04-19 19:39:08 -0700

    initial files

---
+++ src/MainForm.cs
@@ -0,0 +1,16 @@
+﻿namespace EmbeddedReactJS
+{
+    public partial class MainForm : Form
+    {
+        public MainForm()
+        {
+            InitializeComponent();
+            this.Load += MainForm_Load;
+        }
+
+        private void MainForm_Load(object? sender, EventArgs e)
+        {
+            webView21.Source = new Uri("http://localhost:44433", UriKind.Absolute);
+        }
+    }
+}
---
+++ src/Program.cs
@@ -0,0 +1,40 @@
+
+namespace EmbeddedReactJS
+{
+    public class Program
+    {
+        [STAThread]
+        public static void Main(string[] args)
+        {
+            var builder = WebApplication.CreateBuilder(args);
+
+            // Add services to the container.
+
+            builder.Services.AddControllersWithViews();
+
+            var app = builder.Build();
+
+            // Configure the HTTP request pipeline.
+            if (!app.Environment.IsDevelopment())
+            {
+            }
+
+            app.UseStaticFiles();
+            app.UseRouting();
+
+
+            app.MapControllerRoute(
+                name: "default",
+                pattern: "{controller}/{action=Index}/{id?}");
+
+            app.MapFallbackToFile("index.html");
+
+            app.RunAsync();
+
+            Application.SetHighDpiMode(HighDpiMode.SystemAware);
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            Application.Run(new MainForm());
+        }
+    }
+}
